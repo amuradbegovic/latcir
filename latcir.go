21acd97d728e8620384ac58b54755499259c0b9d
Adnan Muradbegović <adnan.muradbegovic@hotmail.com>
2021-12-06 23:11:51 +0100

    Popravio algoritam

--- latcir.go
+++ latcir.go
@@ -14,24 +14,36 @@ func main() {
 	lat := []rune("ABCČĆDĐEFGHIJKLMNOPRSŠTUVZŽabcčćdđefghijklmnoprsštuvzž")
 	cir := []rune("АБЦЧЋДЂЕФГХИЈКЛМНОПРСШТУВЗЖабцчћдђефгхијклмнопрсштувзж")
 	input := []rune(input_raw)
+
+	var digraf bool = false
+
 	for i, c := range input {
 
 		if c == rune('L') && (input[i+1] == rune('J') || input[i+1] == 'j') {
 			fmt.Printf("Љ")
+			digraf = true
 		} else if c == rune('l') && (input[i+1] == rune('J') || input[i+1] == 'j') {
 			fmt.Printf("љ")
+			digraf = true
 		} else if c == rune('N') && (input[i+1] == rune('J') || input[i+1] == 'j') {
 			fmt.Printf("Њ")
+			digraf = true
 		} else if c == rune('n') && (input[i+1] == rune('J') || input[i+1] == 'j') {
 			fmt.Printf("њ")
+			digraf = true
 		} else if c == rune('D') && (input[i+1] == rune('Ž') || input[i+1] == 'ž') {
 			fmt.Printf("Џ")
+			digraf = true
 		} else if c == rune('d') && (input[i+1] == rune('Ž') || input[i+1] == 'ž') {
 			fmt.Printf("џ")
+			digraf = true
 		} else {
-			i++
+			if digraf {
+				digraf = false
+				continue
+			}
 			for k, slovo := range lat {
-				if c == slovo {
+				if input[i] == slovo {
 					fmt.Printf("%c", cir[k])
 				}
 			}
