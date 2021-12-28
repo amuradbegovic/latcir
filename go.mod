1e7679bba3dcac840f3a049c22b72d559456d78c
Adnan Muradbegović <adnan.muradbegovic@hotmail.com>
2021-12-28 23:15:29 +0100

    pretvorio latcir.go u modul

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module amuradbegovic/latcir
+module github.com/amuradbegovic/latcir-go
 
-go 1.16
+go 1.17
--- latcir.go
+++ latcir.go
@@ -1,41 +1,33 @@
-package main
+package latcir
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+func Latcir(input []rune) string  {
 
-func main() {
-
-	reader := bufio.NewReader(os.Stdin)
-	input_raw, _ := reader.ReadString('\n')
 
 	lat := []rune("ABCČĆDĐEFGHIJKLMNOPRSŠTUVZŽabcčćdđefghijklmnoprsštuvzž")
 	cir := []rune("АБЦЧЋДЂЕФГХИЈКЛМНОПРСШТУВЗЖабцчћдђефгхијклмнопрсштувзж")
-	input := []rune(input_raw)
 
+	var output string
 	var digraf bool = false
 
 	for i, c := range input {
 
 		if c == 'L' && (input[i+1] == 'J' || input[i+1] == 'j') {
-			fmt.Printf("Љ")
+			output = output + "Љ"
 			digraf = true
 		} else if c == 'l' && (input[i+1] == 'J' || input[i+1] == 'j') {
-			fmt.Printf("љ")
+			output = output + "љ"
 			digraf = true
 		} else if c == 'N' && (input[i+1] == 'J' || input[i+1] == 'j') {
-			fmt.Printf("Њ")
+			output = output + "Њ"
 			digraf = true
 		} else if c == 'n' && (input[i+1] == 'J' || input[i+1] == 'j') {
-			fmt.Printf("њ")
+			output = output + "њ"
 			digraf = true
 		} else if c == 'D' && (input[i+1] == 'Ž' || input[i+1] == 'ž') {
-			fmt.Printf("Џ")
+			output = output + "Џ"
 			digraf = true
 		} else if c == 'd' && (input[i+1] == 'Ž' || input[i+1] == 'ž') {
-			fmt.Printf("џ")
+			output = output + "џ"
 			digraf = true
 		} else {
 			if digraf {
@@ -44,10 +36,10 @@ func main() {
 			}
 			for k, slovo := range lat {
 				if input[i] == slovo {
-					fmt.Printf("%c", cir[k])
+					output = output + string(cir[k])
 				}
 			}
 		}
 	}
-
+	return output
 }
